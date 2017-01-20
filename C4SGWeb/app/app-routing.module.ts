5b3d68f4c31afeaa427317f5ce3457f9126313f7
ericfzeng <ericfzeng@gmail.com>
2017-01-20 00:14:31 -0600

    Create project setup

--- C4SGWeb/app/app-routing.module.ts
+++ C4SGWeb/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { VolunteerProjectComponent }  from './project/volunteer/volunteer-projec
 import { OrganizationProjectComponent }  from './project/organization/organization-project.component';
 import { ViewProjectComponent }  from './project/view/view-project.component';
 import { SearchProjectComponent }  from './project/search/search-project.component';
+import { CreateProjectComponent }  from './project/create/create.component';
 
 import { AppComponent }  from './app.component';
 import { HomeComponent }  from './home/home.component';
@@ -16,9 +17,12 @@ const routes: Routes = [
     { path: '', component: HomeComponent },
 
     { path: 'projects', component: OrganizationProjectComponent },
+    { path: 'create_project', component: CreateProjectComponent },
+    { path: 'view-project/:id', component: ViewProjectComponent },
+
     { path: 'volunteers', component: VolunteerProjectComponent },
     { path: 'nonprofits', component: OrganizationProjectComponent },
-    { path: 'view-project/:id', component: ViewProjectComponent },
+
 
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
