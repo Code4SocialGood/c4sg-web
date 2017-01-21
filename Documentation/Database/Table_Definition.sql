9e7966d895f599a556c270a738835727e7cf8655
Oleg <olegdev@gmail.com>
2017-01-21 07:45:38 +0200

    fix3

--- Documentation/Database/Table_Definition.sql
+++ Documentation/Database/Table_Definition.sql
@@ -4,68 +4,83 @@ USE `c4sg`;
 -- Table structure for table `application`
 DROP TABLE IF EXISTS `application`;
 CREATE TABLE `application` (
-  `id`          int(11)   NOT NULL AUTO_INCREMENT,
-  `user_id`     int(11)   NOT NULL,
-  `project_id`  int(11)   NOT NULL,
-  `status`      char(1)   NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `status` char(1) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `match_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `delete_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
 );
 
 -- Table structure for table `cause`
 DROP TABLE IF EXISTS `cause`;
 CREATE TABLE `cause` (
-  `id`      int(11)      NOT NULL AUTO_INCREMENT,
-  `cause`   varchar(100) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cause` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 -- Table structure for table `organization`
 DROP TABLE IF EXISTS `organization`;
 CREATE TABLE `organization` (
-  `id`                   int(11)      NOT NULL AUTO_INCREMENT,
-  `name`                 varchar(100) NOT NULL,
-  `url`                  varchar(100) DEFAULT NULL,
-  `logo`                 varchar(100) DEFAULT NULL,
-  `brief_description`    varchar(200) DEFAULT NULL,
-  `detailed_description` varchar(500) DEFAULT NULL,
-  `address1`             varchar(100) DEFAULT NULL,
-  `address2`             varchar(100) DEFAULT NULL,
-  `city`                 varchar(100) DEFAULT NULL,
-  `state`                varchar(100) DEFAULT NULL,
-  `country`              varchar(100) DEFAULT NULL,
-  `zip`                  varchar(100) DEFAULT NULL,
-  `contact_name`         varchar(100) DEFAULT NULL,
-  `contact_phone`        varchar(20)  DEFAULT NULL,
-  `contact_email`        varchar(30)  DEFAULT NULL,
-  `status`               char(1)      NOT NULL DEFAULT 'A',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `url` varchar(100) DEFAULT NULL,
+  `logo` varchar(100) DEFAULT NULL,
+  `briefDescription` varchar(200) DEFAULT NULL,
+  `detailedDescription` varchar(500) DEFAULT NULL,
+  `address1` varchar(100) DEFAULT NULL,
+  `address2` varchar(100) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `zip` varchar(100) DEFAULT NULL,
+  `contact_name` varchar(100) DEFAULT NULL,
+  `contact_phone` varchar(20) DEFAULT NULL,
+  `contact_email` varchar(30) DEFAULT NULL,
+  `status` char(1) NOT NULL DEFAULT 'A',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_by` int(11) DEFAULT '0',
+  `change_time` timestamp NULL DEFAULT NULL,
+  `change_by` int(11) DEFAULT NULL,
+  `delete_time` timestamp NULL DEFAULT NULL,
+  `delete_by` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
 -- Table structure for table `project`
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
-  `id`              int(11)      NOT NULL AUTO_INCREMENT,
-  `name`            varchar(100) NOT NULL,
-  `description`     varchar(500) DEFAULT NULL,
-  `image`           varchar(100) DEFAULT NULL,
-  `organization_id` int(11)      DEFAULT NULL,
-  `address`         varchar(100) DEFAULT NULL,
-  `city`            varchar(100) DEFAULT NULL,
-  `state`           varchar(100) DEFAULT NULL,
-  `country`         varchar(100) DEFAULT NULL,
-  `zip`             varchar(100) DEFAULT NULL,
-  `remote`          char(1)      DEFAULT 'Y',
-  `contact_name`    varchar(100) DEFAULT NULL,
-  `contact_phone`   varchar(20)  DEFAULT NULL,
-  `contact_email`   varchar(30)  DEFAULT NULL,
-  `status`          char(1)      DEFAULT 'A',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `organization_id` int(11) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `zip` varchar(100) DEFAULT NULL,
+  `remote` char(1) DEFAULT 'Y',
+  `contact_name` varchar(100) DEFAULT NULL,
+  `contact_phone` varchar(20) DEFAULT NULL,
+  `contact_email` varchar(30) DEFAULT NULL,
+  `status` char(1) DEFAULT 'A',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_by` int(11) DEFAULT '0',
+  `change_time` timestamp NULL DEFAULT NULL,
+  `change_by` int(11) DEFAULT NULL,
+  `delete_time` timestamp NULL DEFAULT NULL,
+  `delete_by` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
 -- Table structure for table `skill`
 DROP TABLE IF EXISTS `skill`;
 CREATE TABLE `skill` (
-  `id`    int(11)      NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `skill` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 );
@@ -73,22 +88,27 @@ CREATE TABLE `skill` (
 -- Table structure for table `user`
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id`          int(11)      NOT NULL AUTO_INCREMENT,
-  `email`       varchar(100) NOT NULL,
-  `phone`       varchar(100) DEFAULT NULL,
-  `state`       varchar(100) DEFAULT NULL,
-  `country`     varchar(100) DEFAULT NULL,
-  `zip`         varchar(100) DEFAULT NULL,
-  `status`      char(1)      NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `zip` varchar(100) DEFAULT NULL,
+  `status` char(1) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `change_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `delete_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
 );
 
 -- Table structure for table `user_organization`
 DROP TABLE IF EXISTS `user_organization`;
 CREATE TABLE `user_organization` (
-  `id`              int(11) NOT NULL AUTO_INCREMENT,
-  `user_id`         int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `organization_id` int(11) NOT NULL,
-  `status`          char(1) NOT NULL,
+  `status` char(1) NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `delete_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
 );
