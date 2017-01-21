7337f1a597d61439e69ad118f5ffa4d8f9defa8a
Warren McCoy Jr <warren@outtethedev.com>
2017-01-21 14:54:01 -0500

    Login page added with empty forgot password link

--- C4SGWeb/app/app-routing.module.ts
+++ C4SGWeb/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { AppComponent }  from './app.component';
 import { HomeComponent }  from './home/home.component';
 import { AboutComponent }  from './about/about.component';
 import { ContactComponent }  from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
 
@@ -26,6 +27,7 @@ const routes: Routes = [
 
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
+    { path: 'login', component: LoginComponent }
 ];
 @NgModule({
     imports: [ RouterModule.forRoot(routes) ],
