2b312e19c37c89c9a273335cf7aa28e8e53c2f9a
shobhit-sri <ssriva14@asu.edu>
2017-01-25 18:21:46 -0700

    issue50. Added log mechanism using log4j2 and slf4j using AspectJ.

---
+++ C4SGSvc/src/main/java/org/c4sg/logger/ApplicationLogger.java
@@ -0,0 +1,43 @@
+package org.c4sg.logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ApplicationLogger {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	/*@Before("execution(* org.c4sg.controller.ProjectController+.*(..))")
+	public void logProjectController(){
+		logger.info("Calling Before Logger By Aspect");
+	}*/
+	
+	@Pointcut("execution(* org.c4sg.*.*.*(..))")
+    public void defineEntryPoint() {
+    }
+	
+	 @Before("defineEntryPoint()")
+     public void beforeMethod(JoinPoint joinPoint) {
+            logger.info("Before: {}.{}", joinPoint.getTarget().getClass().getSimpleName(), joinPoint.getSignature().getName());
+     }
+	 
+	 @After("defineEntryPoint()")
+     public void afterMethod(JoinPoint joinPoint) {
+		 logger.info("After: {}.{}", joinPoint.getTarget().getClass().getSimpleName(), joinPoint.getSignature().getName());
+     }
+	 
+	 @AfterThrowing(pointcut="defineEntryPoint()", throwing="exception")
+	 public void onThrow(JoinPoint joinPoint, Exception exception){
+		 logger.info("Exception occurred while executing: {}.{}", joinPoint.getTarget().getClass().getSimpleName(), joinPoint.getSignature().getName());
+		 logger.error(exception.getMessage(), exception);
+	 }
+}
