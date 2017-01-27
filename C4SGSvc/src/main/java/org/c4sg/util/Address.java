01fef1a0da17180a2e9d0bee0ff4af0c0b709d03
ramsubbu06 <ramsubbu06@gmail.com>
2017-01-27 02:23:42 -0600

    changes #140 added concatenation instead of Stringbuffer

--- C4SGSvc/src/main/java/org/c4sg/util/Address.java
+++ C4SGSvc/src/main/java/org/c4sg/util/Address.java
@@ -1,30 +1,30 @@
 package org.c4sg.util;
 public class Address {
-	private String doorNo;
-	private String streetName;
+	private String address1;
+	private String address2;
 	private String cityName;
 	private String state;
 	private String zip;
 	private String country;
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
 	public String getCountry() {
 		return country;
 	}
 	public void setCountry(String country) {
 		this.country = country;
 	}
-	public String getDoorNo() {
-		return doorNo;
-	}
-	public void setDoorNo(String doorNo) {
-		this.doorNo = doorNo;
-	}
-	public String getStreetName() {
-		return streetName;
-	}
-	public void setStreetName(String streetName) {
-		this.streetName = streetName;
-	}
-	public String getCityName() {
+		public String getCityName() {
 		return cityName;
 	}
 	public void setCityName(String cityName) {
--- C4SGSvc/src/main/java/org/c4sg/util/CoordinatesUtil.java
+++ C4SGSvc/src/main/java/org/c4sg/util/CoordinatesUtil.java
@@ -2,66 +2,67 @@ package org.c4sg.util;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
-
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Document;
-public class CoordinatesUtil {
-	  //String Google map Constants
-	  public static final String GMAPADDRESS = "http://maps.googleapis.com/maps/api/geocode/xml?address=";
-	  public static final String UTF =  "UTF-8";
-	  public static final String SENSOR = "&sensor=true";
-	  public static final String STATUS = "/GeocodeResponse/status";
-	  public static final String LATITUDE = "//geometry/location/lat";
-	  public static final String LONGITUDE = "//geometry/location/lng";
-		
-	  /* *This method returns Geocode coordinates of the Address object.
-	  * Geocoding is the process of converting addresses (like "1600 Amphitheatre Parkway,
-	  *  Mountain View, CA") into geographic coordinates (like latitude 37.423021 and longitude -122.083739).
-	  *  Please give all the physical Address values for a precise coordinate. 
-	  * */
-	  public static GeoCode getCoordinates(Address address) throws Exception
-	  {
-		GeoCode geo = new GeoCode();
-		String physicalAddress = new StringBuffer()
-				                 .append(address.getDoorNo())
-				                 .append(",")
-				                 .append(address.getStreetName())
-				                 .append(",")
-				                 .append(address.getCityName())
-				                 .append(",")
-				                 .append(address.getState())
-				                 .append("-")
-				                 .append(address.getZip())
-				                 .append(",")
-				                 .append(address.getCountry()).toString();
-		String api = GMAPADDRESS + URLEncoder.encode(physicalAddress, "UTF-8") + SENSOR;
-	    URL url = new URL(api);
-	    HttpURLConnection httpConnection = (HttpURLConnection)url.openConnection();
-	    httpConnection.connect();
-	    int responseCode = httpConnection.getResponseCode();
-	    if(responseCode == 200)
-	    {
-	      DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();;
-	      Document document = builder.parse(httpConnection.getInputStream());
-	      XPathFactory xPathfactory = XPathFactory.newInstance();
-	      XPath xpath = xPathfactory.newXPath();
-	      XPathExpression expr = xpath.compile(STATUS);
-	      String status = (String)expr.evaluate(document, XPathConstants.STRING);
-	      if(status.equals("OK"))
-	      {
-	         expr = xpath.compile(LATITUDE);
-	         String latitude = (String)expr.evaluate(document, XPathConstants.STRING);
-	         expr = xpath.compile(LONGITUDE);
-	         String longitude = (String)expr.evaluate(document, XPathConstants.STRING);
-	         geo.setLatitude(latitude);
-	         geo.setLongitude(longitude);
-	      }
+
+public abstract class CoordinatesUtil {
+   //String Google map Constants
+	public static final String GMAPADDRESS = "http://maps.googleapis.com/maps/api/geocode/xml?address=";
+	public static final String UTF =  "UTF-8";
+	public static final String SENSOR = "&sensor=true";
+	public static final String STATUS = "/GeocodeResponse/status";
+	public static final String LATITUDE = "//geometry/location/lat";
+	public static final String LONGITUDE = "//geometry/location/lng";
+	
+	/**@param{Object} Address- The physical address of a location
+	  * This method returns Geocode coordinates of the Address object.Geocoding is the process of converting addresses (like "1600 Amphitheatre Parkway,
+	  * Mountain View, CA") into geographic coordinates (like latitude 37.423021 and longitude -122.083739).Please give all the physical Address values 
+	  * for a precise coordinate. setting none of the values will give a null value for the Latitude and Longitude. 
+	  */ 
+	public static GeoCode getCoordinates(Address address) throws Exception
+	 {
+	  GeoCode geo = new GeoCode();
+	  String address1="";
+	  String address2="";
+	  String cityName="";
+	  String state="";
+	  String zip="";
+	  String country="";
+	  if(!StringUtils.isEmpty(address.getAddress1())){address1=address.getAddress1();}
+	  if(!StringUtils.isEmpty(address.getAddress2())){address2=address.getAddress2();}
+	  if(!StringUtils.isEmpty(address.getCityName())){cityName=address.getCityName();}
+	  if(!StringUtils.isEmpty(address.getState())){state=address.getState();}
+	  if(!StringUtils.isEmpty(address.getZip())){zip=address.getZip();}
+	  if(!StringUtils.isEmpty(address.getCountry())){country=address.getCountry();}
+	  String physicalAddress = address1+","+address2+","+cityName+","+state+","+zip+","+country;
+	  String api = GMAPADDRESS + URLEncoder.encode(physicalAddress, "UTF-8") + SENSOR;
+	  URL url = new URL(api);
+	  HttpURLConnection httpConnection = (HttpURLConnection)url.openConnection();
+	  httpConnection.connect();
+	  int responseCode = httpConnection.getResponseCode();
+	  if(responseCode == 200)
+	   {
+	    DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+	    Document document = builder.parse(httpConnection.getInputStream());
+	    XPathFactory xPathfactory = XPathFactory.newInstance();
+	    XPath xpath = xPathfactory.newXPath();
+	    XPathExpression expr = xpath.compile(STATUS);
+	    String status = (String)expr.evaluate(document, XPathConstants.STRING);
+	    if(status.equals("OK"))
+	     {
+	       expr = xpath.compile(LATITUDE);
+	       String latitude = (String)expr.evaluate(document, XPathConstants.STRING);
+           expr = xpath.compile(LONGITUDE);
+	       String longitude = (String)expr.evaluate(document, XPathConstants.STRING);
+	       geo.setLatitude(latitude);
+	       geo.setLongitude(longitude);
+	     }
 	    }
 	    else
 	    {
