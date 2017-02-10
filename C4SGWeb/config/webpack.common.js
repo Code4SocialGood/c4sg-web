468e3d484d7619d22db1b98ef180fdf1e2879be6
Tom Johnson <tjohnson@toparstreet.com>
2017-02-10 15:11:37 -0800

    fix logo not showing up in production

--- C4SGWeb/config/webpack.common.js
+++ C4SGWeb/config/webpack.common.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file-loader?name=/app/images/[name].[hash].[ext]'
+        loader: 'file-loader?name=app/images/[name].[hash].[ext]'
       },
       {
         test: /\.css$/,
