3b35d7f6f0440e82e5a6d1ab3160f231507900c6
Tom Johnson <tjohnson@toparstreet.com>
2017-02-10 10:06:51 -0800

    move materialize css out of index.html

--- C4SGWeb/app/main.ts
+++ C4SGWeb/app/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import '../styles.css';
+import 'materialize-css/bin/materialize.css';
 
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
