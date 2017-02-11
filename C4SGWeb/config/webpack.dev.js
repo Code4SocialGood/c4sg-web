a702c741af5d6a0fad567550d144672be835e05e
Tom Johnson <Tom@Toms-MacBook-Air.local>
2017-02-11 15:16:15 -0800

    change client port back to 8000

--- C4SGWeb/config/webpack.dev.js
+++ C4SGWeb/config/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: 'http://localhost:8080/',
+    publicPath: 'http://localhost:8000/',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
