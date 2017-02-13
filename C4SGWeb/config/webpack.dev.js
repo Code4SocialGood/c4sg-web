346bdbc31793ddd9d12f71d561a6df726fe7d319
Tom Johnson <tjohnson@toparstreet.com>
2017-02-13 15:28:45 -0800

    move webpack dev server args to config file, and set to port 3000

--- C4SGWeb/config/webpack.dev.js
+++ C4SGWeb/config/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: 'http://localhost:8000/',
+    publicPath: '/',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
@@ -19,6 +19,8 @@ module.exports = webpackMerge(commonConfig, {
 
   devServer: {
     historyApiFallback: true,
-    stats: 'minimal'
+    stats: 'minimal',
+    inline: true,
+    port: 3000
   }
 });
