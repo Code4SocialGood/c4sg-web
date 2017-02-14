793c4d6cdaa91cd88e455651eae69e302f2f9836
Ryan Thomas <ryanthomas970@gmail.com>
2017-02-14 15:16:12 -0800

    minor style edits

--- C4SGWeb/app/app.module.ts
+++ C4SGWeb/app/app.module.ts
@@ -1,27 +1,27 @@
 import './rxjs-extensions';
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
-import { HttpModule, JsonpModule }    from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
 
-import {MaterializeDirective} from "angular2-materialize";
-import { MaterialModule } from '@angular/material'
+import {MaterializeDirective} from 'angular2-materialize';
+import { MaterialModule } from '@angular/material';
 
 // Home
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent }  from './app.component';
-import { HomeComponent }  from './home/home.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 // Project
-import { VolunteerProjectComponent }  from './project/volunteer/volunteer-project.component';
-import { OrganizationProjectComponent }  from './project/organization/organization-project.component';
-import { ViewProjectComponent }  from './project/view/view-project.component';
-import { SearchProjectComponent }  from './project/search/search-project.component';
-import { CreateProjectComponent }  from './project/create/create.component';
-import { ProjectService }  from './project/project.service';
+import { VolunteerProjectComponent } from './project/volunteer/volunteer-project.component';
+import { OrganizationProjectComponent } from './project/organization/organization-project.component';
+import { ViewProjectComponent } from './project/view/view-project.component';
+import { SearchProjectComponent } from './project/search/search-project.component';
+import { CreateProjectComponent } from './project/create/create.component';
+import { ProjectService } from './project/project.service';
 
-import { AboutComponent }  from './about/about.component';
-import { ContactComponent }  from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './forgot-password/reset-password/reset-password.component';
@@ -32,32 +32,32 @@ import { DashboardComponent } from './user/dashboard/dashboard.component';
 import { OrganizationService} from './user/dashboard/organization.service';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-      MaterialModule,
-    HttpModule,
-    AppRoutingModule
-  ],
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    LoginComponent,
-    ForgotPasswordComponent,
-    ResetPasswordComponent,
-    RegistrationComponent,
-    VolunteerProjectComponent,
-    OrganizationProjectComponent,
-    ViewProjectComponent,
-    SearchProjectComponent,    
-    MaterializeDirective,
-    CreateProjectComponent,
-    DashboardComponent,
-  ],
-  providers: [ ProjectService, OrganizationService ],
-  bootstrap: [ AppComponent ]
+ imports: [
+  BrowserModule,
+  FormsModule,
+  ReactiveFormsModule,
+   MaterialModule,
+  HttpModule,
+  AppRoutingModule
+ ],
+ declarations: [
+  AppComponent,
+  HomeComponent,
+  AboutComponent,
+  ContactComponent,
+  LoginComponent,
+  ForgotPasswordComponent,
+  ResetPasswordComponent,
+  RegistrationComponent,
+  VolunteerProjectComponent,
+  OrganizationProjectComponent,
+  ViewProjectComponent,
+  SearchProjectComponent,
+  MaterializeDirective,
+  CreateProjectComponent,
+  DashboardComponent,
+ ],
+ providers: [ ProjectService, OrganizationService ],
+ bootstrap: [ AppComponent ]
 })
 export class AppModule { }
