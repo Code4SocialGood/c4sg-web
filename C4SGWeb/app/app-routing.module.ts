53b7be7d04e79750c7796c1c6e3cc118048364e5
Ryan Thomas <ryanthomas970@gmail.com>
2017-02-14 17:00:10 -0800

    Minor Style Edits

--- C4SGWeb/app/app-routing.module.ts
+++ C4SGWeb/app/app-routing.module.ts
@@ -1,16 +1,16 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { VolunteerProjectComponent }  from './project/volunteer/volunteer-project.component';
-import { OrganizationProjectComponent }  from './project/organization/organization-project.component';
-import { ViewProjectComponent }  from './project/view/view-project.component';
-import { SearchProjectComponent }  from './project/search/search-project.component';
-import { CreateProjectComponent }  from './project/create/create.component';
+import { VolunteerProjectComponent } from './project/volunteer/volunteer-project.component';
+import { OrganizationProjectComponent } from './project/organization/organization-project.component';
+import { ViewProjectComponent } from './project/view/view-project.component';
+import { SearchProjectComponent } from './project/search/search-project.component';
+import { CreateProjectComponent } from './project/create/create.component';
 
-import { AppComponent }  from './app.component';
-import { HomeComponent }  from './home/home.component';
-import { AboutComponent }  from './about/about.component';
-import { ContactComponent }  from './contact/contact.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './forgot-password/reset-password/reset-password.component';
@@ -20,25 +20,25 @@ import { DashboardComponent } from './user/dashboard/dashboard.component';
 
 const routes: Routes = [
 
-    { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent },
 
-    { path: 'projects', component: OrganizationProjectComponent },
-    { path: 'create_project', component: CreateProjectComponent },
-    { path: 'view-project/:id', component: ViewProjectComponent },
+  { path: 'projects', component: OrganizationProjectComponent },
+  { path: 'create_project', component: CreateProjectComponent },
+  { path: 'view-project/:id', component: ViewProjectComponent },
 
-    { path: 'volunteers', component: VolunteerProjectComponent },
-    { path: 'nonprofits', component: OrganizationProjectComponent },
-    { path: 'dashboard', component: DashboardComponent },
-    
-    { path: 'about', component: AboutComponent },
-    { path: 'contact', component: ContactComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'forgot-password', component: ForgotPasswordComponent },
-    { path: 'reset-password', component: ResetPasswordComponent },
-    { path: 'register' , component: RegistrationComponent }
+  { path: 'volunteers', component: VolunteerProjectComponent },
+  { path: 'nonprofits', component: OrganizationProjectComponent },
+  { path: 'dashboard', component: DashboardComponent },
+
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'register' , component: RegistrationComponent }
 ];
 @NgModule({
-    imports: [ RouterModule.forRoot(routes) ],
-    exports: [ RouterModule ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
