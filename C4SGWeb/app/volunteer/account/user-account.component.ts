a092df198100017674f44345c636b8a0a58cedc9
Chaitanya-Shashidharan <chaitanyas16@yahoo.com>
2017-02-16 15:24:24 -0600

    MyAccount Upload Button

--- C4SGWeb/app/volunteer/account/user-account.component.ts
+++ C4SGWeb/app/volunteer/account/user-account.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,10 @@ import { Validators, FormGroup, FormControl } from '@angular/forms';
 })
 
 export class UserAccountComponent {
+
+  public file_srcs: string[] ;
+  public debug_size_before: string[];
+  public debug_size_after: string[];
 
   public myAccount = new FormGroup({
     username: new FormControl('', Validators.required),
@@ -23,12 +27,83 @@ export class UserAccountComponent {
     confirmPassword: new FormControl('', Validators.required)
   });
 
-  constructor() { }
+  constructor( private changeDetectorRef: ChangeDetectorRef) { }
 
   updateAccount(event) {
     const accountData = this.myAccount.value;
     console.log(event);
     console.log(accountData);
   }
+
+
+  fileChange(input) {
+    this.readFiles(input.files);
+  }
+
+  readFile(file, reader, callback) {
+
+    reader.onload = () => {
+      callback(reader.result);
+    };
+    reader.readAsDataURL(file);
+
+  }
+
+  readFiles(files, index = 0) {
+
+    let reader = new FileReader();
+    if (index in files) {
+
+      this.readFile(files[index], reader, (result) => {
+
+        let img = document.createElement('img');
+
+        img.src = result;
+        this.resize(img, 250, 250, (resized_jpeg, before, after) => {
+          this.debug_size_before = before;
+          this.debug_size_after = after;
+          this.file_srcs = resized_jpeg;
+          this.readFiles(files, index + 1);
+        });
+      });
+    } else {
+
+      this.changeDetectorRef.detectChanges();
+    }
+  }
+
+  resize(img, MAX_WIDTH: number, MAX_HEIGHT: number, callback) {
+    return img.onload = () => {
+
+      let width = img.width;
+      let height = img.height;
+
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+
+      let canvas = document.createElement('canvas');
+
+      canvas.width = width;
+      canvas.height = height;
+      let ctx = canvas.getContext('2d');
+
+      ctx.drawImage(img, 0, 0, width, height);
+
+      let dataUrl = canvas.toDataURL('image/jpeg');
+
+      callback(dataUrl, img.src.length, dataUrl.length);
+
+    };
+  }
+
 
 }
