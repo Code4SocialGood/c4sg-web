a34e9bf9c194095515acd69e8945fdcc1517a649
justindiaw <110058259@qq.com>
2017-02-20 19:56:31 -0500

    Add ts code to control nav-bar style
    Navbar will style dfferently in and off home page.

--- C4SGWeb/app/app.component.ts
+++ C4SGWeb/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
     templateUrl: 'app.component.html'
 })
 
-export class AppComponent {
+export class AppComponent{
+
+	constructor(router: Router){
+		this.router = router;
+	}
+	//control nav style by changing the class name
+	isAtHome() {
+		if(this.router.url ==='/'){
+			return 'at-home'
+		}else{
+			return 'off-home';
+		}
+	}
 }
