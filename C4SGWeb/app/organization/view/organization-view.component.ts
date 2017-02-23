7b439acef5559959d7b4c460a16d29aa7336c828
sean-lloyd <sean.lloyd.accounts@gmail.com>
2017-02-23 11:51:10 -0500

    Organization Page #312

--- C4SGWeb/app/organization/view/organization-view.component.ts
+++ C4SGWeb/app/organization/view/organization-view.component.ts
@@ -1,3 +1,57 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
+
+import { OrganizationService } from '../common/organization.service';
+
+@Component({
+  // moduleId: module.id,
+  selector: 'my-organization',
+  templateUrl: 'organization-view.component.html',
+  styleUrls: ['organization-view.component.css']
+})
+
+export class OrganizationViewComponent implements OnInit, OnDestroy {
+  public myOrganization;
+  public organization;
+  private orgIndex: number;
+  private routeSubscription: Subscription;
+
+  constructor(private organizationService: OrganizationService, private route: ActivatedRoute) {
+    this.getRoute();
+  }
+
+  ngOnInit(): void {
+    this.getOrganization(this.orgIndex);
+  }
+
+  getRoute(): void {
+    this.routeSubscription = this.route.params.subscribe(
+      (params: any) => {
+        this.orgIndex = +params['id'];
+      }
+    );
+  }
+
+  getOrganization(id: number): void {
+    this.organizationService.getOrganization(id).subscribe(
+      (res) => {
+        this.organization = res.json();
+      }, (err) => {
+        console.error('An error occurred', err); // for demo purposes only
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSubscription) { this.routeSubscription.unsubscribe(); }
+  }
+}
+
+// TODO: Move code below to organization-create.component.ts
+
+/**
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
@@ -64,3 +118,5 @@ export class OrganizationViewComponent implements OnInit {
   }
 
 }
+
+ */
