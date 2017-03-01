0749fa8f7962de88943fc93dad048b1d1baed35a
vitshch <vitali.shcherban@gmail.com>
2017-03-01 22:17:48 +0200

    C4SG-229 REST API - User search [2/18]

--- C4SGSvc/src/main/java/org/c4sg/dao/UserDAO.java
+++ C4SGSvc/src/main/java/org/c4sg/dao/UserDAO.java
@@ -1,25 +1,27 @@
 package org.c4sg.dao;
 
+import org.c4sg.constant.Status;
+import org.c4sg.constant.UserRole;
+import org.c4sg.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-import org.c4sg.constant.Status;
-import org.c4sg.constant.UserRole;
-import org.c4sg.entity.Project;
-import org.c4sg.entity.User;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+@Component
+public interface UserDAO extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
-public interface UserDAO extends CrudRepository<User, Long> {
     String FIND_BY_ID_QUERY = "SELECT u FROM UserProject up " +
                                 "JOIN up.user u " +
                                 "JOIN up.project p " +
                                 "WHERE p.id = :projId";
 
-    List<User> findAll();
     //temporary until create date is added
     List<User> findByStatusOrderByUserNameAsc(Status status);
+
     User findById(int id);
 
     List<User> findByRoleAndDisplayFlagOrderByGithubDesc(UserRole role, Boolean display);
