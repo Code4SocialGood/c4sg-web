7266d5ee22345eff6ee3beea68f353dc69acb2bd
Routhana Long <deac.long@wsu.edu>
2017-03-05 03:47:34 -0800

    Add title table and populate with dummy data (V12, V13).

---
+++ C4SGSvc/src/main/resources/db/migration/V13__Data_load_title_data.sql
@@ -0,0 +1,9 @@
+--Populate title table with dummy data
+INSERT INTO `title` (`id`,`title`)
+  VALUES ('1','Business Analyst'),
+         ('2','UI/UX Designer'),
+         ('3','Graphic Designer'),
+         ('4','Developer'),
+         ('5','Quality Assurance'),
+         ('6','Project Manager'),
+         ('7','Technical Writer');
