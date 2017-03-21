24076aa2b345ab859bd138ef5ea94c81c5e89afd
jekcosty <jekcosty@gmail.com>
2017-03-21 11:32:49 -0400

    Correcting user role for ADMIN
    ADMIN role should have not been qualified with a prefix of C4SG as it
    is not necessary

--- src/app/user/common/user.service.ts
+++ src/app/user/common/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import { Observable } from 'rxjs';
 import { User } from './user';
 import { environment } from '../../../environments/environment';
