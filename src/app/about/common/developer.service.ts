dbb0248133e317863a2bbcc6357b5f95ad15ea0d
hameleonka <hameleonka@yandex.ru>
2017-03-22 14:19:28 +0300

    Fixes #59 About Us Page - Clickable Avatar

--- src/app/about/common/developer.service.ts
+++ src/app/about/common/developer.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { environment } from '../../../environments/environment';
 
-const developersUrl = `${environment.backend_url}/api/users`;
+const developersUrl = `${environment.backend_url}/api/users/developers`;
 
 @Injectable()
 export class DeveloperService {
