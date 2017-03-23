b91459586ce907985fb406884dbf61d14ddb004a
dvicente04 <dvicente04@alumni.depaul.edu>
2017-03-23 18:51:43 -0500

    logo image display and uploader service

--- src/app/organization/common/organization.service.ts
+++ src/app/organization/common/organization.service.ts
@@ -47,14 +47,14 @@ export class OrganizationService {
 
   saveLogo(organizationId: number, fileContent: string): Observable<Response>{
       return this.http.post(
-      `${organizationUrl}/${organizationId}/uploadLogo`, 
+      `${organizationUrl}/${organizationId}/logo`, 
       { "": fileContent }
       );
   }
 
   retrieveLogo(organizationId: number): Observable<Response>{
       return this.http.get(
-      `${organizationUrl}/${organizationId}/getLogo`
+      `${organizationUrl}/${organizationId}/logo`
       )
   }
   
--- src/app/organization/view/organization-view.component.ts
+++ src/app/organization/view/organization-view.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
 
 import { OrganizationService } from '../common/organization.service';
-
+import { UploaderService } from '../../_services/uploader.service';
 @Component({
   // moduleId: module.id,
   selector: 'my-organization',
@@ -13,22 +13,25 @@ import { OrganizationService } from '../common/organization.service';
 
 export class OrganizationViewComponent implements OnInit, OnDestroy {
   public myOrganization;
-  public organization;
+  public organization: any = {};
   private orgIndex: number;
   private routeSubscription: Subscription;
 
-  constructor(private organizationService: OrganizationService, private route: ActivatedRoute) {
+  constructor(private organizationService: OrganizationService,  
+    private route: ActivatedRoute, 
+    private uploader:UploaderService,) {
     this.getRoute();
+    this.organization.logo = '';
   }
 
   ngOnInit(): void {
-    this.getOrganization(--this.orgIndex);
+    this.getOrganization(this.orgIndex);
   }
 
   getRoute(): void {
     this.routeSubscription = this.route.params.subscribe(
       (params: any) => {
-        this.orgIndex = +params['id'];
+        this.orgIndex = +params['organizationId'];
       }
     );
   }
@@ -36,11 +39,17 @@ export class OrganizationViewComponent implements OnInit, OnDestroy {
   getOrganization(id: number): void {
     this.organizationService.getOrganization(id).subscribe(
       (res) => {
-        this.organization = res.json();
-      }, (err) => {
-        console.error('An error occurred', err); // for demo purposes only
+        const org = res.json()
+        org.logo = ''
+        this.organization = org
+        this.uploader.displayImage(id,
+            this.organizationService.retrieveLogo.bind(this.organizationService),
+            (img: any) => this.organization.logo = img)
+      }, 
+      (err) => {
+        console.error('An error occurred', err); 
       }
-    );
+    )
   }
 
   ngOnDestroy(): void {
