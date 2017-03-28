92badbc9b52ea869178d6359694650e44ba1cf54
hameleonka <hameleonka@yandex.ru>
2017-03-28 13:52:19 -0400

    Fixes #46 Organization Detail Page - Display Opportunities Panel

--- src/app/organization/view/organization-view.component.ts
+++ src/app/organization/view/organization-view.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs/Rx';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs/Rx';
 
-import { OrganizationService } from '../common/organization.service';
+import {OrganizationService} from '../common/organization.service';
+import {Project} from '../../project/common/project';
+import {ProjectService} from '../../project/common/project.service';
 
 @Component({
   // moduleId: module.id,
@@ -16,26 +18,28 @@ export class OrganizationViewComponent implements OnInit, OnDestroy {
   public organization;
   private orgIndex: number;
   private routeSubscription: Subscription;
+  projects: Project[];
 
-  constructor(private organizationService: OrganizationService, private route: ActivatedRoute, 
-    private router: Router) {
+  constructor(private organizationService: OrganizationService, private projectService: ProjectService, private route: ActivatedRoute,
+              private router: Router) {
     this.getRoute();
   }
 
   ngOnInit(): void {
-    this.getOrganization(--this.orgIndex);
+    this.getOrganization(this.orgIndex);
+    this.getProjects(this.orgIndex);
   }
 
   getRoute(): void {
     this.routeSubscription = this.route.params.subscribe(
       (params: any) => {
-        this.orgIndex = +params['id'];
+        this.orgIndex = +params['organizationId'];
       }
     );
   }
 
   getOrganization(id: number): void {
-    this.organizationService.getOrganization(id).subscribe(
+    this.organizationService.getOrganization(id + 1).subscribe(
       (res) => {
         this.organization = res.json();
       }, (err) => {
@@ -44,11 +48,27 @@ export class OrganizationViewComponent implements OnInit, OnDestroy {
     );
   }
 
+  getProjects(id: number): void {
+    this.projectService.getProjectByOrg(id).subscribe(
+      res => {
+        this.projects = res.json();
+        this.projects.forEach((project) => {
+          if (project.description.length > 100) {
+            project.description = project.description.slice(0, 100) + "...";
+          }
+        });
+      },
+      error => console.log(error)
+    );
+  }
+
   edit(organization): void {
     this.router.navigate(['/nonprofit/edit', 2]);
   }
 
   ngOnDestroy(): void {
-    if (this.routeSubscription) { this.routeSubscription.unsubscribe(); }
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
   }
 }
