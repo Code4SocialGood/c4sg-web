8faa5fae1a489536e80bdc28c3b715c2ceea9c23
dvicente04 <dvicente04@alumni.depaul.edu>
2017-04-03 10:08:47 -0500

    filename adjustment

--- src/app/user/account/user-account.component.ts
+++ src/app/user/account/user-account.component.ts
@@ -190,7 +190,7 @@ export class UserAccountComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    let id = this.route.snapshot.params['userId'] - 1;
+    let id = this.route.snapshot.params['userId'];
     this.imageDisplay.displayImage(id,
             this.userService.retrieveAvatar.bind(this.userService))
             .subscribe(res => this.avatar = res.url)
