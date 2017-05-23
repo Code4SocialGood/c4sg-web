87d0a29935f81097577101a566217d9bdc909bc8
Code for Social Good <code4socialgood@gmail.com>
2017-05-23 13:36:08 -0500

    Revert "Revert "Retrieve skills #302""

--- src/app/user/list/user-list.component.ts
+++ src/app/user/list/user-list.component.ts
@@ -116,6 +116,14 @@ export class UserListComponent implements OnInit, OnDestroy {
         // and the total number of data rows
         this.users = res;
         this.totalItems = res.length;
+          res.forEach((e: User) => {
+            this.skillService.getSkillsForUser(e.id).subscribe(
+                result => {
+                    e.skills = result;
+                    console.log(e.skills);
+                       });
+                 });
+
       },
       error => console.error(error)
       );
