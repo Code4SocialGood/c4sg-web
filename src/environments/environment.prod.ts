f173fd6f388ec4d2c950ea3277e5aadbe1460b43
jekcosty <jekcosty@gmail.com>
2017-06-11 22:46:36 -0400

    AWS S3 integration
    added env for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,11 @@ export const environment = {
     production: true,
     backend_url: 'http://localhost:8080',
     auth_clientID: '332DZAQiUiXAXYYKvzJy6EodylS2rze5',
-    auth_domain: 'c4sg.auth0.com'
+    auth_domain: 'c4sg.auth0.com',
+    AWS_ACCESS_KEY_ID: 'AKIAJCZUH6MBDLYBK5CQ',
+    AWS_SECRET_ACCESS_KEY: '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',
+    AWS_IMAGE_BUCKET: 'c4sg.images',
+    AWS_DOCS_BUCKET: 'c4sg.docs',
+    AWS_REGION: 'us-west-2',
+    AWS_SIGNATURE_VERSION: 'v4'
 };
