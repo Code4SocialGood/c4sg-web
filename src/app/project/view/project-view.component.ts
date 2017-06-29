510ba0a6b9dc92483729b8332415d6d47b95e946
joshisayali <sayali.j23@gmail.com>
2017-06-29 13:44:46 -0700

    #602 - Login prompt added when clicked on Bookmark

--- src/app/project/view/project-view.component.ts
+++ src/app/project/view/project-view.component.ts
@@ -181,8 +181,8 @@ export class ProjectViewComponent implements OnInit {
                 }
             );
     } else {
-        // display toast when user is not logged in
-        this.globalActions.emit({action: 'toast', params: ['Please login to add bookmark', 4000]});
+        localStorage.setItem('redirectAfterLogin', this.router.url);
+        this.authService.login();
     }
   }
 
