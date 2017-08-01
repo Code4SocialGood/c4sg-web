546f985e88eae1583aac0c766ce56444c45506aa
Hamza Hajji <hmzhjji@gmail.com>
2017-08-01 20:54:40 +0100

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/Rx';
 import { Project } from '../project/common/project';
 import { ProjectService } from '../project/common/project.service';
 import {DataService} from '../_services/data.service';
-import { AuthService } from './../auth.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'my-home',
