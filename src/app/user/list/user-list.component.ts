513222c402cae0a8be4222bcf08b4511c6c7c95f
Hamza Hajji <hmzhjji@gmail.com>
2017-08-02 00:02:58 +0100

    fix description length issue

--- src/app/user/list/user-list.component.ts
+++ src/app/user/list/user-list.component.ts
@@ -50,6 +50,14 @@ export class UserListComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute) {
   }
 
+  slice(str: string): string {
+    var maxLength = 200;
+    var trimmedString = str.substr(0, maxLength);
+
+    //re-trim if we are in the middle of a word
+    return trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
+  }
+
   ngOnInit(): void {
     this.route.params.subscribe(
       params => {
