995b454e194f2abb47aac98ff85161485c3d3058
Kiranpreet <kiranpreet@gettripmate.com>
2017-09-24 01:14:44 -0500

    Resolved lint issue

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'stories',
+  selector: 'our-stories',
   templateUrl: './stories.component.html',
   styleUrls: ['./stories.component.scss']
 })
