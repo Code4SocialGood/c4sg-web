0d8d54ae59e0d781169537a4cce2359ce855ccef
joshisayali <sayali.j23@gmail.com>
2017-10-02 12:41:19 -0700

    Creating new object for Application

--- src/app/project/common/application.ts
+++ src/app/project/common/application.ts
@@ -1,5 +1,7 @@
 export class Application {
   constructor(
+    public projectId: int,
+    public userId: int,
     public status: string,   
     public resumeFlag: boolean,   
     public comment?: string
