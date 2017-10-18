6dad89f3850f3e7152788247d30902a4eb8d6dff
Felipe A L Pereira <felipealves.gnu@gmail.com>
2017-10-17 22:48:03 -0300

    Fix build issue

---
+++ src/environments/environment.prod-remote.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    backend_url: 'https://c4sg-prod.herokuapp.com/',
+    auth_clientID: '332DZAQiUiXAXYYKvzJy6EodylS2rze5',
+    auth_domain: 'c4sg.auth0.com',
+    auth_tenant_shared: false,
+    auth_api: 'https://c4sg-api',
+    auth_callback_env: 'prod',
+    auth_silenturl: 'silent.html',
+    AWS_IMAGE_BUCKET: 'c4sg.images',
+    AWS_DOCS_BUCKET: 'c4sg.docs',
+    AWS_REGION: 'us-west-2',
+    AWS_SIGNATURE_VERSION: 'v4'
+};
