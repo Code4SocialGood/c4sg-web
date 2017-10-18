584b4342cbf5c11dee64526563f0249d7a6995b7
Felipe A. L. Pereira <felipe.alves@sigma.com.br>
2017-10-18 14:33:28 -0300

    Add new profile that enables running backend from heroku dev environment

---
+++ src/environments/environment.dev-heroku.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    backend_url: 'https://c4sg-dev.herokuapp.com',
+    auth_clientID: 'oO8PRySIJnGHDGsZQ36zOhhccNE6iNjf',
+    auth_domain: 'c4sg-dev.auth0.com',
+    auth_tenant_shared: false,
+    auth_api: 'https://c4sg-api',
+    auth_callback_env: 'local',
+    auth_silenturl: 'silent-dev-local.html',
+    AWS_IMAGE_BUCKET: 'c4sg.dev.images',
+    AWS_DOCS_BUCKET: 'c4sg.dev.docs',
+    AWS_REGION: 'us-west-2',
+    AWS_SIGNATURE_VERSION: 'v4'
+};
