12b01ba1fa9afffa64a643b1d23311e82653ee96
Trishul Mukherjee <elflord@Trishuls-MacBook-Pro.local>
2017-11-19 02:10:28 -0500

    Fixed lint and build issues

--- src/app/project/common/input.formatter.directive.ts
+++ src/app/project/common/input.formatter.directive.ts
@@ -1,14 +1,14 @@
 import {Directive, EventEmitter, HostListener, Output} from '@angular/core';
 @Directive({
-  selector: '[ngModel][inputFormatter]'
+  selector: '[ngModel][myInputFormatter]'
 })
 export class InputFormatterDirective {
   @Output() ngModelChange: EventEmitter<any> = new EventEmitter();
   value: any;
 
   @HostListener('input', ['$event']) onInputChange($event) {
-    //convert the input to lowercase and replace any ' '(space) with a '-'
-    this.value = $event.target.value.toLowerCase().replace(/ /g,"-");
+    // convert the input to lowercase and replace any ' '(space) with a '-'
+    this.value = $event.target.value.toLowerCase().replace(/ /g, '-');
     this.ngModelChange.emit(this.value);
   }
 }
