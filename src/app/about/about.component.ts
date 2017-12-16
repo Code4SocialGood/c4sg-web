8498601ffea031420e84318cecdabedff25dabdb
Lekshmi Sasidharan <lekshmiajith11@gmail.com>
2017-12-16 16:13:09 -0500

    issue solved #1761 about us page (#1767)
    * issue solved #1761 about us page
    
    * issue solved #1761 about us page
    
    * issue solved #1761 about us page
    
    * issue solved #1761 about us page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'my-about',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(
+  constructor(public authSvc: AuthService
   ) { }
 
   ngOnInit(): void {
   }
 }
+
